f8d55f5225ea5072968d520853f71f89ea0de0b6
ziqi <ziqi_mo@163.com>
2024-05-11 01:30:41 +0800

    feat: update feed

--- app/chat/consumers.py
+++ app/chat/consumers.py
@@ -1,10 +1,12 @@
 import asyncio
 import os
 import aiohttp
+import redis.asyncio as aioredis 
 import json
 from urllib.parse import parse_qs
 
 from django.core.cache import cache
+
 from asgiref.sync import sync_to_async
 from channels.db import database_sync_to_async
 from channels.generic.websocket import AsyncWebsocketConsumer
@@ -39,6 +41,17 @@ class ChatConsumer(AsyncWebsocketConsumer):
             self.room.members.all()
         ):
             raise PermissionDenied("You are not allowed to access this chat room.")
+        
+        self.user_id = str(self.scope["user"].id)
+        
+        self.redis = aioredis.Redis(
+            host=os.getenv('REDIS_HOST'),
+            port=os.getenv('REDIS_PORT'),
+            # password=os.getenv('REDIS_PASSWORD'),
+            db=0,
+            encoding="utf-8",
+            decode_responses=True
+        )
 
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
         await self.accept()
@@ -49,6 +62,8 @@ class ChatConsumer(AsyncWebsocketConsumer):
         asyncio.create_task(self.safe_task(self.event_push()))
 
     async def disconnect(self, close_code):
+        await self.redis.close()
+
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
     async def init(self):
@@ -65,7 +80,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
         while True:
             await asyncio.sleep(10)
             if datetime.now() - self.last_ping_time > timeout:
-                logger.warning("No Ping received for 5 minutes, disconnecting...")
+                logger.warning(f"No Ping received from user_{self.user_id} for 5 minutes, disconnecting...")
                 await self.close()
                 break
 
@@ -85,7 +100,6 @@ class ChatConsumer(AsyncWebsocketConsumer):
             await self.send(json.dumps({'type': 'lang', 'data': self.language}))
             return
 
-        self.user_id = self.scope["user"].id
         room = await database_sync_to_async(ChatRoom.objects.get)(pk=self.room_name)
 
         message_obj = Message(user=self.scope["user"], content=text_data, room=room)
@@ -96,20 +110,18 @@ class ChatConsumer(AsyncWebsocketConsumer):
             {
                 "type": "chat_message",
                 "message": text_data,
-                "user_id": str(self.user_id),
+                "user_id": self.user_id,
             },
         )
 
     async def chat_message(self, event):
-        connected_user_model = self.scope["user"]
-        user_id = str(connected_user_model.id)
         message = event["message"]
         try:
             message = json.loads(message)
         except:
             pass
         
-        await self.send(text_data=json.dumps({"type": "message", "data": message, "user_id": user_id}, ensure_ascii=False))
+        await self.send(text_data=json.dumps({"type": "message", "data": message, "user_id": self.user_id}, ensure_ascii=False))
 
     async def safe_task(self, coro):
         try:
@@ -117,21 +129,18 @@ class ChatConsumer(AsyncWebsocketConsumer):
         except Exception as e:
             logger.error(f"Uncaught exception: {e}")
 
-    async def get_subscriptions(self, user_id):
-        cache_key = f'satoshi:user_subscriptions_{user_id}'
+    async def get_subscriptions(self):
+        cache_key = f'satoshi:subscriptions:{self.user_id}'
         subscriptions = cache.get(cache_key)
         if not subscriptions:
-            subscriptions = await database_sync_to_async(list)(UserSubscription.objects.filter(user_id=user_id))
+            subscriptions = await database_sync_to_async(list)(UserSubscription.objects.filter(user_id=self.user_id))
             cache.set(cache_key, subscriptions, timeout=60)
         return subscriptions
 
     async def event_push(self):
-        user_id = str(self.scope["user"].id)
-
-        sent_data = set()
         while True:
             try:
-                subscriptions = await self.get_subscriptions(user_id)
+                subscriptions = await self.get_subscriptions()
                 if subscriptions:
                     subscriptions_dict = {}
                     for subscription in subscriptions:
@@ -141,26 +150,28 @@ class ChatConsumer(AsyncWebsocketConsumer):
                             content = subscription.content
                         subscriptions_dict[MESSAGE_TYPE_DICT[subscription.message_type]] = content
 
-                    data = await self.get_event(user_id, subscriptions_dict)
+                    data = await self.get_event(subscriptions_dict)
 
-                    new_data = [news for news in data if news["id"] not in sent_data]   
-                    sent_data.update([news["id"] for news in new_data])
+                    new_data = []
+                    for news in data:
+                        feed_id = f"{news['data_type']}_{news['id']}"
+                        key = f'satoshi:feed:{self.user_id}:{feed_id}'
+                        
+                        if not await self.redis.exists(key):
+                            await self.redis.set(key, 1, ex=86400)
+                            new_data.append(news)
+                   
                     if new_data:
                         await self.send(text_data=json.dumps({"type": "event", "data": new_data}, ensure_ascii=False))
-
-                    # if data:
-                    #     await self.send(text_data=json.dumps({"type": "event", "data": data}, ensure_ascii=False))
-                    #     await asyncio.sleep(10)
-                    # await asyncio.sleep(30)
             except Exception as e:
                 logger.error(f'Event push: {e}')
             finally:
                 await asyncio.sleep(3)
 
-    async def get_event(self, user_id, subscriptions_dict):
+    async def get_event(self, subscriptions_dict):
         async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(120)) as session:
             url = f"{os.getenv('SUB_API')}/message_post/"
-            response_text = await self.fetch(session, url, dict(user_id=user_id, language=self.language, content=subscriptions_dict))
+            response_text = await self.fetch(session, url, dict(user_id=self.user_id, language=self.language, content=subscriptions_dict))
 
         data = json.loads(response_text).get('data', [])
         return data
